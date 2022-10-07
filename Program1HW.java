9b5ec33d857124dc6cea3391c926c92d5eac9978
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-07 22:43:34 +0530

    Program1HW.java

---
+++ Program1HW.java
@@ -0,0 +1,9 @@
+package Java_1_Get_Started;
+// 1. First Java Program
+
+public class Program1HW {
+	public static void main(String[] args) {
+		System.out.println("You Are Doing Well Abhay");
+	}
+
+}
