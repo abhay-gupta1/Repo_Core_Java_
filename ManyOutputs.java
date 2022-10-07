16e41dd320d19adf28e0e1c7d03893b77c215e19
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-07 22:44:03 +0530

    Add files via upload

---
+++ ManyOutputs.java
@@ -0,0 +1,12 @@
+package Java_2_Output;
+/* 2. You can add as many println() methods as you want.
+ s Note that it will add a new line for each method:
+ */
+
+public class ManyOutputs {
+	public static void main(String[] args) {
+		System.out.println("Hello Abhay");
+		System.out.println("Have A Nice Day!");
+		System.out.println("Good Luck Buddy");
+	}
+}
