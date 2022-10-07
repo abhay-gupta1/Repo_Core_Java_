9283a221b46a5e293c0db762ad304c15907849cb
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-07 22:46:07 +0530

    Delete MathCal.java

--- MathCal.java
+++
@@ -1,9 +0,0 @@
-package Java_2_Output;
-/*3. You can also output numbers, and perform mathematical calculations:
- * Note :- we don't use double quotes ("") inside println() to output numbers.
- */
-public class MathCal {
-	public static void main(String[] args) {
-		System.out.println(4 * 4);
-	}
-}
