46bc7f813950dcadadde3ffdfa6d8d1c6a5b7381
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-08 23:25:37 +0530

    Boolean

---
+++ BooleanExpression.java
@@ -0,0 +1,19 @@
+package Java_9_Boolean;
+/*A Boolean expression is a Java expression that returns a Boolean value: true or false.
+
+You can use a comparison operator, such as the greater than (>) operator to find out if an expression (or a variable) is true:*/
+
+public class BooleanExpression {
+	public static void main(String[] args) {
+		int x = 8;
+		int y =998;
+		System.out.println(x > y);
+		
+		System.out.println(98 < 124); // or even easier
+		
+		int z = 34;
+		System.out.println(z == 34);  // In the examples below, we use the equal to (==) operator to evaluate an expression:
+		
+		System.out.println(98 == 76);  
+	}
+}
---
+++ BooleanValues.java
@@ -0,0 +1,12 @@
+package Java_9_Boolean;
+
+/*A boolean type is declared with the boolean keyword and can only take the values true or false:*/
+
+public class BooleanValues {
+	public static void main(String[] args) {
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		System.out.println(isJavaFun);   // Output True
+		System.out.println(isFishTasty); // Output False
+	}
+}
