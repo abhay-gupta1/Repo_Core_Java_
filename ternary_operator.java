7ff27581136d34a98dc73145f46d651c646c54a5
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-23 12:01:57 +0530

    Conditional Statements

---
+++ ternary_operator.java
@@ -0,0 +1,12 @@
+package Conditional_Statements_8;
+
+public class ternary_operator {
+	public static void main(String[] args) {
+		
+		int number = 4;
+		
+		String type = (number % 2 == 0) ? "even" : "odd";
+		
+		System.out.println(type);
+	}
+}
