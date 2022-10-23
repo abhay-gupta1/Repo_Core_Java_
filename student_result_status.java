8a6388d06330baf9c0162fe001124f8648328f08
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-23 12:02:33 +0530

    Conditional Statements

---
+++ student_result_status.java
@@ -0,0 +1,12 @@
+package Conditional_Statements_8;
+
+public class student_result_status {
+	public static void main(String[] args) {
+		
+		int marks = 66;
+		
+		String status = (marks >= 33)? "Pass" : "Fail";
+		
+		System.out.println(status);
+	}
+}
