6431001da61d9a089a0951006f5f1bee46d90cb0
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-23 12:02:13 +0530

    Conditional Statements

---
+++ switch_statement.java
@@ -0,0 +1,26 @@
+package Conditional_Statements_8;
+
+public class switch_statement {
+	public static void main(String[] args) {
+		
+		int number = 2;
+		
+		switch(number) {
+		
+		case 1:
+			System.out.println("Samosa");
+			break;
+			
+		case 2 :
+			System.out.println("Burger");
+			break;
+			
+		case 3:
+			System.out.println("Mango Shake");
+			break;
+		
+		default:
+			System.out.println("I realize I am dreaming");
+		}
+	}
+}
