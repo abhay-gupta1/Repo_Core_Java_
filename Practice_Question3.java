d0cdc52f3f5ef4198ad2280a4a7d8774a7b75e42
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-29 22:39:10 +0530

    Loops_

--- Practice_Question3.java
+++ Practice_Question3.java
@@ -1,15 +1,27 @@
-package Operators_7;
+package Loops_Flow_Control_9;
 
+// Write a program to find the factorial of any number entered by the user.
+import java.util.*;
 public class Practice_Question3 {
 	public static void main(String[] args) {
 		
-		int x, y, z;
+		Scanner sc = new Scanner(System.in);
 		
-		x = y = z = 2;
-		x += y;
-		y -= z;
-		z /= (x + y);
+		int num; // To hold number
 		
-		System.out.println(x + " " + y + " " + z);
+		int fact = 1; // To hold factorial
+		
+		System.out.println("Enter any positive integer:");
+		
+		num = sc.nextInt();
+		
+		
+		for(int i = 1; i <= num; i++) {
+			//fact *= i;
+			
+			fact = fact * i;
+		}
+		
+		System.out.println("Factorial:" +" "+fact);
 	}
 }
