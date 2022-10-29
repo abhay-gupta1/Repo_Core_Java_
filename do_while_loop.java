056aee7edcf24b511e72ebcb386e96c07a4cb8d1
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-29 22:34:47 +0530

    Loops_

---
+++ do_while_loop.java
@@ -0,0 +1,14 @@
+package Loops_Flow_Control_9;
+
+public class do_while_loop {
+	public static void main(String[] args) {
+		
+		int counter = 1;
+		
+		do {
+			System.out.println("Hello World!");
+			counter ++;
+		}
+		while(counter <= 10);
+	}
+}
---
+++ for_loop.java
@@ -0,0 +1,12 @@
+package Loops_Flow_Control_9;
+
+public class for_loop {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++) {
+			
+			System.out.println("Hello World!");
+		}
+	}
+	
+}
