7ab934d21727fa8d269b09bc1f22fe4bdcee2ce1
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-29 22:39:40 +0530

    Loops_

--- Practice_Question5.java
+++ Practice_Question5.java
@@ -1,14 +1,26 @@
-package Operators_7;
+package Loops_Flow_Control_9;
+
+// What is wrong in the following program?
 
 public class Practice_Question5 {
 	public static void main(String[] args) {
 		
-		int x = 10, y =5;
+		for(int i=0;i<=5;i++ ) {
+			
+			System.out.println("i = "+ i);
+		}
 		
-		int exp1 = (y * (x / y + x / y));
-		int exp2 = (y * x / y + y * x / y);
-		
-		System.out.println(exp1);
-		System.out.println(exp2);
+		//System.out.println("i after the loop = "+ i );
 	}
 }
+
+/*Scope of variable is referred to the part of the program where the variable can be used.
+ * 
+ * In this program variable i is declared in the for loop.So scope of variable
+ * i is limited to the for loop only that is between { and } of the for loop.
+ * There is a display statement after the for loop where variable i is
+ *  used which means i is used out of scope. This leads to compilation errors.
+ *  The program given will not run and give an error instead.To correct the program, the variable i needs to be declared outside
+ *   the for loop
+ * */
+ 
