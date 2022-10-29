ba186665a238af30e10d23ef2816fd392030cf03
Abhay Gupta <105590117+abhay-gupta1@users.noreply.github.com>
2022-10-29 22:37:10 +0530

    Loops_

---
+++ While_Loop.java
@@ -0,0 +1,16 @@
+package Loops_Flow_Control_9;
+
+public class While_Loop {
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		
+		while(counter < 100) {
+			
+			System.out.println("Hello World!");
+			
+			counter ++;
+		}
+		System.out.println("Printed HW 100 times");
+	}
+}
